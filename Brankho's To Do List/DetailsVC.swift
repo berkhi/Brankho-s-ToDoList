602c7ffcda332e9e30661b64db10ffe013d39c12
Berk Hayta <berkhayta@hotmail.com>
2023-02-08 16:30:43 +0300

    Features added (save, edit, delete, listing)

--- Brankho's To Do List/DetailsVC.swift
+++ Brankho's To Do List/DetailsVC.swift
@@ -6,23 +6,85 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsVC: UIViewController {
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var detailsLabel: UILabel!
+    @IBOutlet weak var notesLabel: UILabel!
+    var chosenNote = ""
 
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        detailsLabel.isHidden = true
+        
+        if chosenNote != "" {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ToDoList")
+            
+            fetchRequest.predicate = NSPredicate(format: "note = %@", self.chosenNote)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+              let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        if let note = result.value(forKey: "note") as? String{
+                            textView.text = note
+                            textField.isHidden = true
+                            let barButton = self.navigationItem.rightBarButtonItem
+                            barButton?.isEnabled = false
+                            notesLabel.isHidden = true
+                            detailsLabel.isHidden = false
 
+                        }
+                    }
+                }
+            }catch{
+                
+            }
+        }
+        
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func saveButton(_ sender: Any) {
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newNote = NSEntityDescription.insertNewObject(forEntityName: "ToDoList", into: context)
+        newNote.setValue(textField.text, forKey: "note")
+        /*
+        let event = NSEntityDescription.insertNewObject(forEntityName: "ToDoList", into: context)
+
+
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let dateTime = dateFormatter.string(from: date)
+
+        event.setValue(dateTime, forKeyPath: "createdDate")
+*/
+        do {
+            try context.save()
+            print("Save success")
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
         
+        NotificationCenter.default.post(name: NSNotification.Name("newData"), object: nil)
+        self.navigationController?.popViewController(animated: true)
         
+
+    }
+    @objc func hideKeyboard(){
+        view.endEditing(true)
     }
     
 }
--- Brankho's To Do List/ViewController.swift
+++ Brankho's To Do List/ViewController.swift
@@ -6,17 +6,53 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-    @IBOutlet weak var tableView: UITableView!
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
     
+    @IBOutlet weak var tableView: UITableView!
+    var noteArray = [String]()
+    var selectedNote = ""
+    var chosenNote = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        view.addGestureRecognizer(gestureRecognizer)
+        tableView.delegate = self
+        tableView.dataSource = self
+        getData()
+        
         
     }
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newData"), object: nil)
+    }
+    
+    @objc func getData(){
+        
+        noteArray.removeAll(keepingCapacity: false)
+
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ToDoList")
+        
+        do{
+          let results =  try context.fetch(fetchRequest)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject]{
+                    if let note = result.value(forKey: "note") as? String{
+                        self.noteArray.append(note)
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+        }catch{
+            print("error")
+        }
+       
+    }
+    
 
     @IBAction func addButton(_ sender: Any) {
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(buttonAction))
@@ -24,11 +60,70 @@ class ViewController: UIViewController {
 
     }
     @objc func buttonAction(){
+        selectedNote = ""
+        performSegue(withIdentifier: "goToDetails", sender: nil)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return noteArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = noteArray[indexPath.row]
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToDetails" {
+            let destinationVC = segue.destination as! DetailsVC
+            destinationVC.chosenNote = selectedNote
+            
+        }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedNote = noteArray[indexPath.row]
         performSegue(withIdentifier: "goToDetails", sender: nil)
     }
     
-    @objc func hideKeyboard(){
-        view.endEditing(true)
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ToDoList")
+            chosenNote = noteArray[indexPath.row]
+            fetchRequest.predicate = NSPredicate(format: "note = %@", self.chosenNote)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let  chosenNoteDelete = result.value(forKey: "note") as? String {
+                            if chosenNoteDelete == noteArray[indexPath.row] {
+                                context.delete(result)
+                                noteArray.remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                
+                                do{
+                                   try context.save()
+                                }catch{
+                                    print("Error while reloading table")
+                                }
+                                
+                                break
+                                
+                            }
+                        }
+                    }
+                }
+            }catch{
+                print("Error while deleting")
+            }
+        }
     }
+
+
 }
 
